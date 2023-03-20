5a67ef50b720a348163d0dc6e98bc59958fda2fc
Kernelpost <kernelpost@gmail.com>
2023-03-20 13:37:39 +0300

    Move to support ffmpeg4.4

--- cmake/ffmpeg-compat/cfg_av_pix_fmt_vdpau.c
+++ cmake/ffmpeg-compat/cfg_av_pix_fmt_vdpau.c
@@ -1,4 +1,4 @@
-#include <libavcodec/avcodec.h>
+#include <ffmpeg4.4/libavcodec/avcodec.h>
 int main(void) {
     return AV_PIX_FMT_VDPAU;
 }
--- cmake/ffmpeg-compat/cfg_avcodecflags.c
+++ cmake/ffmpeg-compat/cfg_avcodecflags.c
@@ -1,4 +1,4 @@
-#include <libavcodec/avcodec.h>
+#include <ffmpeg4.4/libavcodec/avcodec.h>
 int main(void) {
     return AV_CODEC_FLAG_TRUNCATED + AV_CODEC_CAP_TRUNCATED;
 }
